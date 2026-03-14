2e65a70b2e3e296d5868fe027f2835a97a4c0e0f
Hideart <hidecrowd@gmail.com>
2026-03-14 14:16:21 +0400

    feat(ci): add release-please for auto versioning and changelog
    - Add release-please config: auto CHANGELOG.md generation from
      conventional commits, semver bump (feat=minor, fix=patch, breaking=major)
    - Add release workflow: release-please creates Release PR on main,
      GoReleaser builds binaries on tag
    - Add PR title linter: enforce conventional commit format on PR titles
      with project-specific scopes
    - Add internal/version package: version constant managed by release-please
    - Remove manual changelog enforcer (superseded by release-please)
    - Wire version package into cmd/bepro/main.go

--- cmd/bepro/main.go
+++ cmd/bepro/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
-)
 
-var version = "dev"
+	"github.com/Hideart/bepro/internal/version"
+)
 
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "--version" {
-		fmt.Println("bepro", version)
+		fmt.Println("bepro", version.Version)
 		os.Exit(0)
 	}
 
-	fmt.Println("bepro", version)
+	fmt.Println("bepro", version.Version)
 }
